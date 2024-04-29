3c9112bf9bd614f102d22bf18fbb1fc1f1beeecd
Suchkov Dmitry <45110271+Koobi33@users.noreply.github.com>
2024-04-29 20:15:38 -0300

    fix

--- src/common/utils/envConfig.ts
+++ src/common/utils/envConfig.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const env = cleanEnv(process.env, {
   NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
-  BOT_SECRET: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
+  BOT_SECRET: str({ devDefault: testOnly('test') }),
   HOST: host({ devDefault: testOnly('localhost') }),
   PORT: port({ devDefault: testOnly(3000) }),
   CORS_ORIGIN: str({ devDefault: testOnly('http://localhost:3000') }),
