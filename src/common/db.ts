2f5ef539345005793c0a337c6819a6cf7ae03026
i.saley <i.saley@greengreystudio.com>
2024-04-30 21:05:05 +0300

    fixed passwd)))))))))))))

--- src/common/db.ts
+++ src/common/db.ts
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+import { env } from '@/common/utils/envConfig';
 
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'postgres',
-  password: 'Ro0TU$$eR_n1gGa',
+  password: env.BD_KEY,
   port: 5432, // Default PostgreSQL port
 });
 
